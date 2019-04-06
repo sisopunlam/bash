043bb8b01b66bde6ed9372ef7d1c7a1145e27c30
webber0 <42701029+webber0@users.noreply.github.com>
2019-04-06 19:47:00 -0300

    Primer Commit

---
+++ ejercicio1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Los errores van a /dev/null
+#Limpiando la pantalla
+clear
+#Las respuestas a la parte teorica
+#a)¿Qué significa la linea '#!/bin/bash'?
+#"La primera linea '#!/bin/bash' se denomina she bang y es para indicar 
+#a la terminal que interprete debe utilizar para interpretar lo que sigue. 
+#En este caso, indica que 'lo que sigue' se tiene que leer con BASH"
+
+#b)¿Con qué comando y de que manera otorgó los permisos de ejecución al script?
+#Los permisos de ejecución se otorgaron con el siguiente comando:
+#chmod 755 ./(nombre del script).sh
+#en donde '755' indica (en base octal) que: el owner tiene permisos de leer/escribir/ejecutar (rwx)
+#el script y, tanto el grupo al que pertenece el owner como otros usuarios tienen permisos de leer/ejecutar el script (r-x)
+
+#c)Explique el objetivo general del script
+#(Explicar)
+
+#e) ¿Es posible ejecutar este script para procesar varios archivos a la vez en una única llamada? Ejemplifique
+#Saludando.
+echo "Hola $USER! Ahora el script va a resolver el Ejercicio 1 del Trabajo Practico de BASH"
+
+#Para avisar que devuelvo el control del sistema
+echo "¡Gracias por esperar, ahora, te devuelvo el control!"
