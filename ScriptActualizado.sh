013f02079339bd424087c40432e08dfd70aadd0e
Juan Pablo Del Greco <42810855+juanpablodelgreco@users.noreply.github.com>
2019-04-18 14:17:37 -0300

    Actualizado
    Le puse el resumen detallado, falta poner lo de cantidad de contribuyentes filtrados. Y agregar rutas relativas , absolutas y con espacios

--- ScriptActualizado.sh
+++ ScriptActualizado.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 if test $# -gt 2;
 then
-	echo "HAY MAS DE 2 PARAMETROS"
+	echo "Hay mas de 2 parametros"
+	echo "Por favor ingresa -h para consultar la ayuda"
 	exit 0
 fi
 
-if [ "$1" == "-h" ] && [ $# == 1 ]; then
+if  [ "$1" == "-h" ] && [ $# == 1 ] ; then
+	echo "A partir de un archivo de texto, este script filtra por CUIT, DNI y nombre"
 	echo "Argumentos del script: <(-c,-d,-n)> <File>"
+	echo "Si usted invoca <File> se mostrara en detalle toda la informacion contenida en el archivo"
 	echo "-c) Filtra por Cuit"
 	echo "-d) Filtra por Dni"
 	echo "-n) Filtra por Nombre"
@@ -14,27 +17,41 @@ if [ "$1" == "-h" ] && [ $# == 1 ]; then
 
 fi
 
+if [ -f "$1" ]; then
+	awk 'BEGIN {
+	FIELDWIDTHS = " 11 30 2 2 2  1 1 2"
+	print "CUIT\t	DENOMINACION\t\t\tGAN\tIVA\tMON\tINT\tEMP\tACT.MON"
+	}
+	{
+	print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8
+	 }' $1
+exit	
+fi
+
 if [ ! -f "$2" ]; then
 
-	echo "NO ES UN FILE"
+	echo "El segundo argumento no es un file"
+	echo "Por favor ingresa -h para consultar la ayuda"
 	exit
 fi
 
 if [ $# != 2 ]; then
-	echo "TIENE UN SOLO ARGUMENTO"
+	echo "Tiene un solo argumento"
+	echo "Por favor, ingresa -h para consultar la ayuda"
 	exit
 fi
 
 if [ "$1" == "-c" ]; then
 
 	#echo FILTRO POR CUIT
-	awk 'BEGIN { 
+	awk 'BEGIN {
+		print "CUIT" 
 		FIELDWIDTHS = "11"
+		
+
 	}
 {
-	print "CUIT"
 	print $1
-	print "----"
 
 }' $2
 exit
@@ -46,10 +63,11 @@ if [ "$1" == "-d" ]; then
 	#echo FILTRO POR DNI
 	awk 'BEGIN { 
 		FIELDWIDTHS = "1 9"
+		print "DNI"
 	}
-	{	print "DNI"	
+	{		
 		print $2
-		print "---"
+	
 	} ' $2
 exit
 fi
@@ -59,12 +77,17 @@ if [ "$1" == "-n" ]; then
 	#echo FILTRO POR NOMBRE
 	awk 'BEGIN {
 	FIELDWIDTHS = "11 30"
+	print "NOMBRE"
 	}
 	 {
-	print "Nombre"
 	print $2
-	print "------"
+	
 	
 }' $2
 	exit
+fi
+
+if [ "$1" != "-c" ] || [ "$1" != "-n" ] || [ "$1" != "-d" ]; then
+echo "Parametro no valido, por favor ingresa -h para consultar la ayuda"
+exit 
 fi
