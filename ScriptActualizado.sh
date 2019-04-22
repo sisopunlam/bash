626a76f4b4988e1913a4bf282cd68ab5defeb447
webber0 <42701029+webber0@users.noreply.github.com>
2019-04-21 22:35:51 -0300

    Mejora al codigo anterior
    +Bajo el listado general de un archivo de texto, mejore el formateado de este, con tabulaciones adecuadas a cada campo
    +Agregé un contador cuando creí conveniente (era necesario), sino, me guie por la cantidad de registros
    +Mejore el filtrado bajo la logica "Si la denominación empieza con "Sucesión" no es una persona -> Ese registro no tiene un DNI valido"
    + Reduje a una llamada de 'echo' cuando era posible hacerlo
    +Indique de que forma era mejor la visualización del script

--- ScriptActualizado.sh
+++ ScriptActualizado.sh
@@ -7,13 +7,14 @@ then
 fi
 
 if  [ "$1" == "-h" ] && [ $# == 1 ] ; then
-	echo "A partir de un archivo de texto, este script filtra por CUIT, DNI y nombre"
-	echo "Argumentos del script: <(-c,-d,-n)> <File>"
-	echo "Si usted invoca <File> se mostrara en detalle toda la informacion contenida en el archivo"
-	echo "-c) Filtra por Cuit"
-	echo "-d) Filtra por Dni"
-	echo "-n) Filtra por Nombre"
-	exit
+	echo "A partir de un archivo de texto, este script filtra por CUIT, DNI y nombre
+Argumentos del script: <(-c,-d,-n)> <File>
+Si usted invoca <File> se mostrara en detalle toda la informacion contenida en el archivo
+-c) Filtra por Cuit
+-d) Filtra por Dni
+-n) Filtra por Nombre
+Este script se muestra mejor con la pantalla completa"
+	exit 0
 
 fi
 
@@ -21,17 +22,19 @@ if [ -f "$1" ]; then
 	awk 'BEGIN {
 	FIELDWIDTHS = " 11 30 2 2 2  1 1 2"
 	print "CUIT\t	DENOMINACION\t\t\tGAN\tIVA\tMON\tINT\tEMP\tACT.MON"
+	print "-----------\t------------------------------\t\t---\t---\t--\t---\t-------"
 	}
 	{
-	print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8
-	 }' $1
-exit	
+	print substr($0,1,11)"\t"substr($0,12,30)"\t"substr($7,2,2)"\t"substr($7,4,2)"\t"substr($7,6,2)"\t"substr($7,7,1)"\t"substr($7,8,1)"\t"substr($7,9)	 
+	}
+	END { print "La cantidad de elementos filtrados es: "NR} ' $1
+exit 0
 fi
 
 if [ ! -f "$2" ]; then
 
-	echo "El segundo argumento no es un file"
-	echo "Por favor ingresa -h para consultar la ayuda"
+	echo "El segundo argumento no es un archivo valido
+	Por favor ingresa -h para consultar la ayuda"
 	exit
 fi
 
@@ -46,15 +49,14 @@ if [ "$1" == "-c" ]; then
 	#echo FILTRO POR CUIT
 	awk 'BEGIN {
 		print "CUIT" 
-		FIELDWIDTHS = "11"
-		
-
+		print "-----------"
+		FIELDWIDTHS = "11"	
 	}
-{
-	print $1
-
-}' $2
-exit
+	{
+	print substr($1,1,11)
+	}
+END { print "La cantidad de elementos filtrados es: "NR} ' $2
+exit 0
 fi
 
 
@@ -64,11 +66,17 @@ if [ "$1" == "-d" ]; then
 	awk 'BEGIN { 
 		FIELDWIDTHS = "1 9"
 		print "DNI"
+		print "---------"
+		resultados=0
 	}
 	{		
-		print $2
+		if(substr($1,12)!="SUCESION"){
+			resultados++		
+			print substr($1,3,8);
+		}
 	
-	} ' $2
+	}
+END { print "La cantidad de elementos filtrados es: "resultados} ' $2
 exit
 fi
 
@@ -78,12 +86,18 @@ if [ "$1" == "-n" ]; then
 	awk 'BEGIN {
 	FIELDWIDTHS = "11 30"
 	print "NOMBRE"
+	print "------------------------------"
+	resultados=0
 	}
 	 {
-	print $2
+	if(substr($1,12)!="SUCESION"){
+		print substr($0,12,30)
+		resultados++;
+	}
 	
 	
-}' $2
+}
+END { print "La cantidad de elementos filtrados es: "resultados} ' $2
 	exit
 fi
 
