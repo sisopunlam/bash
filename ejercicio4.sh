7f9e58942c3390ea5185449808c3ad801249740c
Federico Agustin Annacondia <42748808+agusannacondia@users.noreply.github.com>
2019-04-25 22:29:18 -0300

    Update ejercicio4.sh

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -15,23 +15,24 @@ if ! [[ -f "$2" || -d "$2" ]]; then
         exit
 fi
  
-# Si el segundo parametro es un archivo:
- 
-if [ -f "$2" ]; then
+coincidencia() {
         totales=$(cut -d " " -f 1 <(wc -l $1))
         repeticiones=$(cut -d " " -f 1 <(wc -l <(comm -12 <(sort $1) <(sort $2))))
         porcentaje=$(($repeticiones*100/$totales))
-        echo "Hay " $repeticiones " repeticiones de " $totales " palabras totales"
-        echo "Hay un " $porcentaje "% de coincidencia"
-fi
+        echo $1 $2 $porcentaje
+}
  
+# Si el segundo parametro es un archivo:
+ 
+if [ -f "$2" ]; then
+        coincidencia $1 $2
+fi
  
 # Si el segundo parametro es un directorio:
  
 if [ -d "$2" ]; then
- 
-        echo "Nada"
- 
+        for i in $(ls $2)
+        do
+                coincidencia $1 $i
+        done
 fi
- 
-echo "Todo OK"
