bebeb05a1adf963efa7979498113a5b9a0965516
webber0 <42701029+webber0@users.noreply.github.com>
2019-04-27 16:11:04 -0300

    Update
    +Coloco nuestros nombres
    +Coloco un ejemplo

--- ScriptActualizado.sh
+++ ScriptActualizado.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+
+#-----------------------------------------------------------------------#
+# Nombre del Script: Ejercicio2.sh					#
+# Trabajo Práctico Nº1							#
+# Integrantes:								#
+#-Amato, Luciano			DNI:  	40.378.763		#
+#-Pompeo, Nicolas Ruben			DNI:	37.276.705		#
+#-Annacondia, Federico Agustin		DNI:	38.435.945		#
+#-Del Greco, Juan Pablo			DNI:	39.097.812		#
+# Entrega 29/04/2019							#
+#-----------------------------------------------------------------------#
+
+#Evaluo que no sea el caso de que haya muchos parametros
 if test $# -gt 3;
 then
 	echo "Hay mas de 3 parametros"
 	echo "Por favor ingresa -h para consultar la ayuda"
 	exit 0
 fi
-
+#Invocando a la ayuda
 if  [ "$1" == "-h" ] && [ $# == 1 ] ; then
+	echo "Ejercicio n°2  - Trabajo Practico n°1 BASH"
 	echo  	"A partir de un archivo de texto, este script filtra por CUIT, DNI y Nombre"
 	echo	 "Argumentos del script: <(-c,-d,-n)> <File> <(CUIT,DNI,NOMBRE)>"
 	echo	"-c) Filtra por Cuit"
 	echo 	"-d) Filtra por Dni"
 	echo 	"-n) Filtra por Nombre"
-echo "Si usted invoca <File> o <(-c,-d,-n> <File> ,se mostrara en detalle toda la informacion contenida en el archivo"
+	echo "Si usted invoca <File> o <(-c,-d,-n> <File> ,se mostrara en detalle toda la informacion contenida en el archivo"
+	echo "Ejemplo queriendo filtrar por CUIT: ./ejercicio2.sh -c ./caso_base 20000004759"
 	exit 0
 
 fi
